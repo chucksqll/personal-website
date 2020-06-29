be5706bb2b954c2c743a6685cc0dd50551725ba2
Witold Banach <witoldbanach99@gmail.com>
2020-06-29 23:38:12 +0200

    Fixed background color

--- django_project/settings.py
+++ django_project/settings.py
@@ -25,7 +25,6 @@ SECRET_KEY =os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-
 ALLOWED_HOSTS = ['witoldbanach.herokuapp.com','localhost']
 
 
