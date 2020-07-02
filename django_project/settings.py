f8bff002397e14171b8a641e7c838fab45327b82
Witold Banach <witoldbanach99@gmail.com>
2020-07-02 13:08:49 +0200

    added responsive images, delted unnecessary files

--- django_project/settings.py
+++ django_project/settings.py
@@ -20,10 +20,11 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'sjp*t@3fm+$=!yn1c*6643gdp(t0#v$-i$3o70)jmz*yy#dl+8'
+
 SECRET_KEY =os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
+# DEBUG = os.environ.get('DEBUG_VALUE')
 DEBUG = True
 ALLOWED_HOSTS = ['witoldbanach.herokuapp.com','localhost']
 
